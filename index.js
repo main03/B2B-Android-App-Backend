ee1460c0efd5ec3b82c37415240cad0ee74ec57f
main03 <107945158+main03@users.noreply.github.com>
2022-09-20 15:30:23 +0500

    A small bug
    1 small bug detected

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ mongooose.connect(
 );
 const admin = require("./Routes/Admin");
 const retailer = require("./Routes/Retailer");
-
 const categories = require("./Routes/Categories")
 const products=require("./Routes/Product")
-// const orders=require("./Routes/Orders")
 const AdminOrders=require("./Routes/AdminOrders/Orders")
 const RetailerOrders=require("./Routes/RetailerOrder/Order")
 const upcomingoffers=require("./Routes/Offers")
@@ -23,19 +21,15 @@ const bodyparser = require("body-parser");
 app.use(bodyparser.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
-
 app.use('/category', categories)
 app.use('/product',products)
 app.use('/order',AdminOrders)
-app.use('/order',RetailerOrders)
+app.use('/orders',RetailerOrders)
 app.use('/upcomingoffers',upcomingoffers)
 app.use('/region',regions)
 app.use('/admin',admin)
 app.use('/retailer',retailer)
-
-
-
 console.log("Mongo DB Cloud Atlas Connected Successfullyy.....");
-app.listen(process.env.PORT || process.env.BACKEND_PORT , () => console.log("Server running at http://localhost:"+ process.env.BACKEND_PORT));
+app.listen(process.env.PORT || process.env.BACKEND_PORT , () => console.log("Server running at PORT :"+ process.env.BACKEND_PORT));
 
 
