34f0db6042918e2940792f8ddc3c7cb45b469866
main03 <107945158+main03@users.noreply.github.com>
2022-09-20 16:35:01 +0500

    CORS ERROR
    CORS ERROR

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const regions=require("./Routes/Region")
 
  
 const app = express();
+app.use(cors());
 app.use((req,res,next)=>
 {
 res.setHeader('Access-Control-Allow-Origin', '*');
@@ -25,11 +26,10 @@ res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, D
 res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 next();
 
-
 })
 const bodyparser = require("body-parser");
 app.use(bodyparser.json());
-app.use(cors());
+
 app.use("/uploads", express.static("uploads"));
 app.use('/category', categories)
 app.use('/product',products)
